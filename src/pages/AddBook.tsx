10a2a1681748d73d4253886618a61c7e0ff5fbbf
Juwel Rana <juwel.jem@gmail.com>
2025-07-02 19:56:44 +0600

    add book added

--- src/pages/AddBook.tsx
+++ src/pages/AddBook.tsx
@@ -1,19 +1,23 @@
-
 import { useForm, type SubmitHandler } from "react-hook-form";
 import type { IBook } from "@/types";
 import { useEditBookMutation } from "@/redux/api/baseApi";
 import Swal from "sweetalert2";
 import { toastify } from "@/utils/alerts";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
-
-export function EditBookModal() {
+export function AddBook() {
   const [editBook] = useEditBookMutation();
 
   const form = useForm<IBook>();
-
 
   const handleSubmit: SubmitHandler<IBook> = async (data) => {
     const bookData = {
@@ -60,62 +64,93 @@ export function EditBookModal() {
           )}
         />
 
-        <FormField
-          control={form.control}
-          name="author"
-          rules={{ required: "Author name is required!" }}
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel>Author</FormLabel>
-              <FormControl>
-                <Input {...field} value={field.value || ""} />
-              </FormControl>
-              {fieldState.error && (
-                <p className="text-red-500 text-sm mt-1">
-                  {fieldState.error.message}
-                </p>
-              )}
-            </FormItem>
-          )}
-        />
+        {/* Author and ISBN */}
+        <div className="grid grid-cols-2 gap-8">
+          <FormField
+            control={form.control}
+            name="author"
+            rules={{ required: "Author name is required!" }}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>Author</FormLabel>
+                <FormControl>
+                  <Input {...field} value={field.value || ""} />
+                </FormControl>
+                {fieldState.error && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {fieldState.error.message}
+                  </p>
+                )}
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="genre"
-          rules={{ required: "Genre is required!" }}
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel>Genre</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              {fieldState.error && (
-                <p className="text-red-500 text-sm mt-1">
-                  {fieldState.error.message}
-                </p>
-              )}
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="isbn"
+            rules={{ required: "ISBN number is required!" }}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>ISBN Number</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                {fieldState.error && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {fieldState.error.message}
+                  </p>
+                )}
+              </FormItem>
+            )}
+          />
+        </div>
 
-        <FormField
-          control={form.control}
-          name="isbn"
-          rules={{ required: "ISBN number is required!" }}
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel>ISBN Number</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              {fieldState.error && (
-                <p className="text-red-500 text-sm mt-1">
-                  {fieldState.error.message}
-                </p>
-              )}
-            </FormItem>
-          )}
-        />
+        {/* Genre and copies */}
+        <div className="grid grid-cols-2 gap-8">
+          <FormField
+            control={form.control}
+            name="genre"
+            rules={{ required: "Genre is required!" }}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>Genre</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                {fieldState.error && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {fieldState.error.message}
+                  </p>
+                )}
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="copies"
+            rules={{
+              required: "Copies is required",
+              min: {
+                value: 1,
+                message: "Copies must be positive number.",
+              },
+            }}
+            render={({ field, fieldState }) => (
+              <FormItem>
+                <FormLabel>Copies</FormLabel>
+                <FormControl>
+                  <Input type="number" {...field} />
+                </FormControl>
+                {fieldState.error && (
+                  <p className="text-red-500 text-sm mt-1">
+                    {fieldState.error.message}
+                  </p>
+                )}
+              </FormItem>
+            )}
+          />
+        </div>
 
         <FormField
           control={form.control}
@@ -135,32 +170,9 @@ export function EditBookModal() {
             </FormItem>
           )}
         />
-
-        <FormField
-          control={form.control}
-          name="copies"
-          rules={{
-            required: "Copies is required",
-            min: {
-              value: 0,
-              message: "Copies must be 0 or positive number.",
-            },
-          }}
-          render={({ field, fieldState }) => (
-            <FormItem>
-              <FormLabel>Copies</FormLabel>
-              <FormControl>
-                <Input type="number" {...field} />
-              </FormControl>
-              {fieldState.error && (
-                <p className="text-red-500 text-sm mt-1">
-                  {fieldState.error.message}
-                </p>
-              )}
-            </FormItem>
-          )}
-        />
       </Form>
+      <Button className="w-full ml-auto">Add Book</Button>
     </form>
   );
 }
+export default AddBook;
